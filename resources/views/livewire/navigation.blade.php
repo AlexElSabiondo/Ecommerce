67e1cb6a4da70a026e3995b7c426f7f4a4df5309
AlexElSabiondo <alexingenierocierva@gmail.com>
2022-02-01 21:00:20 +0100

    PDF 9

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -90,7 +90,7 @@ cursor-pointer font-semibold h-full">
                 <ul class="bg-white">
                     @foreach($categories as $category)
                         <li class="navigation-link text-trueGray-500 hover:bg-orange-500 hover:text-white">
-                            <a href="" class="py-2 px-4 text-sm flex items-center">
+                            <a href="{{ route('categories.show', $category) }}" class="py-2 px-4 text-sm flex items-center">
                     <span class="flex justify-center w-9">
                         {!! $category->icon !!}
                     </span>
@@ -117,7 +117,7 @@ cursor-pointer font-semibold h-full">
             <ul class="bg-white">
                 @foreach($categories as $category)
                     <li class="text-trueGray-500 hover:bg-orange-500 hover:text-white">
-                        <a href="" class="py-2 px-4 text-sm flex items-center">
+                        <a href="{{ route('categories.show', $category) }}" class="py-2 px-4 text-sm flex items-center">
                             <span class="flex justify-center w-9">
                                 {!! $category->icon !!}
                             </span>
