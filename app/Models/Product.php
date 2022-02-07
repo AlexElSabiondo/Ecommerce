f67b8f8ce59d25da21f5812e0d97e96bf5f9077c
AlexElSabiondo <alexingenierocierva@gmail.com>
2022-02-07 16:21:45 +0100

    PDF 10

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -37,4 +37,9 @@ class Product extends Model
     {
         return $this->morphMany(Image::class, 'imageable');
     }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
